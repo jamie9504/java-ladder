749d0f2ec193bb4bf24f125a057846fdddd83ae8
jamie9504 <jamie9504@kakao.com>
2020-02-15 21:57:32 +0900

    [기능 개선] 참가자 이름을 알려줌

--- src/main/java/domain/ParticipantName.java
+++ src/main/java/domain/ParticipantName.java
@@ -5,7 +5,7 @@ public class ParticipantName {
     private final String name;
 
     public ParticipantName(String name) {
-        this.name = name;
+        this.name = name.trim();
     }
 
     public String getName() {
