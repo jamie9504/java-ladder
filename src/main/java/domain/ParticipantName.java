6724199e55b8a3c33008fa3103f1f9847c997d8b
jamie9504 <jamie9504@kakao.com>
2020-02-15 22:02:27 +0900

    [기능 추가] 참가자 이름 예외 발생

--- src/main/java/domain/ParticipantName.java
+++ src/main/java/domain/ParticipantName.java
@@ -5,7 +5,15 @@ public class ParticipantName {
     private final String name;
 
     public ParticipantName(String name) {
-        this.name = name.trim();
+        name = name.trim();
+        vaildNameLength(name);
+        this.name = name;
+    }
+
+    private static void vaildNameLength(String name) {
+        if (name.length() < 1 || name.length() > 5) {
+            throw new IllegalArgumentException("참가자 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 
     public String getName() {
--- src/test/java/domain/ParticipantNameTest.java
+++ src/test/java/domain/ParticipantNameTest.java
@@ -11,7 +11,7 @@ public class ParticipantNameTest {
 
     @DisplayName("참가자 이름을 알려줌, 단 앞 뒤 공백은 제거할 것")
     @ParameterizedTest
-    @ValueSource(strings = {"제이미", " 제이미", "제이미 "})
+    @ValueSource(strings = {"제이미", " 제이미", "제이미 ", "  제이미    "})
     void getParticipantName(String name) {
         ParticipantName participantName = new ParticipantName(name);
         assertThat(participantName.getName()).isEqualTo(name.trim());
