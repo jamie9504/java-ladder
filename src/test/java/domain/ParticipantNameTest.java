c41c43b2668eb6e434fa5daf7fcf263317caee1f
jamie9504 <jamie9504@kakao.com>
2020-02-15 21:58:53 +0900

    [테스트 추가] 참가자 이름 예외 발생

--- src/test/java/domain/ParticipantNameTest.java
+++ src/test/java/domain/ParticipantNameTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,5 +15,14 @@ public class ParticipantNameTest {
     void getParticipantName(String name) {
         ParticipantName participantName = new ParticipantName(name);
         assertThat(participantName.getName()).isEqualTo(name.trim());
+    }
+
+    @DisplayName("참가자 이름이 1글자 미만이거나 5글자를 초과인 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "제일미제이미"})
+    void exceptionParticipantName(String name) {
+        assertThatThrownBy(() -> new ParticipantName(name))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("이름은");
     }
 }
