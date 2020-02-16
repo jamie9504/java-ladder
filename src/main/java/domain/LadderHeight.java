5a1cfa44e1f75260ab7d9e8b50e8a6cf0a594db5
jamie9504 <jamie9504@kakao.com>
2020-02-16 20:20:54 +0900

    [기능 구현] 사다리 높이 가져오기

---
+++ src/main/java/domain/LadderHeight.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class LadderHeight {
+
+    private final int height;
+
+    public LadderHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
