e4bd39646a495c8c1d8791d712751ec597367bfc
jamie9504 <jamie9504@kakao.com>
2020-02-16 19:45:44 +0900

    [테스트 추가] 상품 개수 확인

--- src/test/java/domain/ProductFactoryTest.java
+++ src/test/java/domain/ProductFactoryTest.java
@@ -2,19 +2,32 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProductFactoryTest {
-    @DisplayName("상품을 셋팅하고, 잘 셋팅했는지 확인 - 셋팅시 참여자 인원을 보내줌")
-    @Test
-    void getParticipantNames() {
+
+    private static ProductFactory productFactory;
+    @BeforeAll
+    static void setProducts() {
         String names = "제일미,  제이미,제삼미, 제 사 미, 제오미 ";
-        ProductFactory productFactory = new ProductFactory(names, 5);
+        productFactory = new ProductFactory(names, 5);
+    }
+
+    @DisplayName("상품이 잘 셋팅했는지 확인")
+    @Test
+    void getProductNames() {
         assertThat(productFactory.getNames().get(0)).isEqualTo("제일미");
         assertThat(productFactory.getNames().get(1)).isEqualTo("제이미");
         assertThat(productFactory.getNames().get(2)).isEqualTo("제삼미");
         assertThat(productFactory.getNames().get(3)).isEqualTo("제 사 미");
         assertThat(productFactory.getNames().get(4)).isEqualTo("제오미");
+    }
+
+    @DisplayName("상품의 개수를 확인")
+    @Test
+    void getProductCounts() {
+        assertThat(productFactory.getSize()).isEqualTo(5);
     }
 }
