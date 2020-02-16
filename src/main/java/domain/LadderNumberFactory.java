aead3e94cc198b3dd2f332e09fd052e03a70bce5
jamie9504 <jamie9504@kakao.com>
2020-02-16 16:54:04 +0900

    [기능 구현] 사다리 개수는 2 이상이여야 함

--- src/main/java/domain/LadderNumberFactory.java
+++ src/main/java/domain/LadderNumberFactory.java
@@ -8,7 +8,14 @@ public class LadderNumberFactory {
     private final List<LadderNumber> ladderNumbers;
 
     public LadderNumberFactory(int count) {
+        validNumber(count);
         ladderNumbers = makeLadderNumbers(count);
+    }
+
+    private void validNumber(int count) {
+        if(count < 2) {
+            throw new IllegalArgumentException("사다리는 " + 2 + "개 이상이여야 합니다.");
+        }
     }
 
     private List<LadderNumber> makeLadderNumbers(int count) {
