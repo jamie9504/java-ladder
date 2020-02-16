bcce576a9f5352f00588105c1490ac58e7e4dd9d
jamie9504 <jamie9504@kakao.com>
2020-02-16 20:11:15 +0900

    [테스트 추가] 작은지 비교

--- src/test/java/domain/ProductFactoryTest.java
+++ src/test/java/domain/ProductFactoryTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProductFactoryTest {
-
+    private static ParticipantFactory participantFactory;
     private static ProductFactory productFactory;
 
     @BeforeAll
     static void setProducts() {
         String names = "제일미,  제이미,제삼미, 제 사 미, 제오미 ";
-        ParticipantFactory participantFactory = new ParticipantFactory(names);
+        participantFactory = new ParticipantFactory(names);
         productFactory = new ProductFactory(names, participantFactory);
     }
 
@@ -37,12 +37,23 @@ public class ProductFactoryTest {
     @DisplayName("참여인원보다 상품이 더 많으면 예외")
     @Test
     void exceptionMoreParticipantFactory() {
-        String namesParticipant = "제일미,  제이미,제삼미, 제 사 미, 제오미";
-        ParticipantFactory participantFactory = new ParticipantFactory(namesParticipant);
-        String namesProduct = "제일미,  제이미,제삼미, 제 사 미, 제오미 , 제유김";
+        String namesProduct = "제일미,  제이미,제삼미, 제 사 미, 제오미 , 제육미";
 
         assertThatThrownBy(() -> new ProductFactory(namesProduct, participantFactory))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("많습니다.");
+    }
+
+    @DisplayName("참여인원보다 상품이 적으면 True")
+    @Test
+    void checkLessThan() {
+        assertThat(productFactory.isLessThan(ParticipantFactory)).isFalse();
+
+        String namesProduct = "제일미,  제이미,제삼미, 제 사 미";
+        ProductFactory productFactory = new ProductFactory(namesProduct, participantFactory);
+
+        assertThat(productFactory.isLessThan(ParticipantFactory)).isTrue();
+
+
     }
 }
