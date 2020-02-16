64de301abac73b998a7575cc958ead6cc289f4d7
jamie9504 <jamie9504@kakao.com>
2020-02-16 20:42:04 +0900

    [기능 추가] 참가자가 2명보다 적을 경우

--- src/main/java/domain/ParticipantFactory.java
+++ src/main/java/domain/ParticipantFactory.java
@@ -7,11 +7,14 @@ import java.util.List;
 public class ParticipantFactory implements ItemFactory {
 
     private final static String SPLIT_DELIMITER = ",";
+    private final static int MORE_THAN_OR_EQUAL_TO_NUMBER = 2;
+    private final static String EXCEPTION_MESSAGE_FORMAT_LESS_THAN = "참가인원(%d명)은 최소 %d명 이상이어야 합니다.";
 
     private final List<Participant> participants;
 
     public ParticipantFactory(String names) {
         List<String> splitNames = split(names);
+        validCountMoreThanOrEqualTo(splitNames);
         this.participants = makeParticipantName(splitNames);
     }
 
@@ -19,12 +22,21 @@ public class ParticipantFactory implements ItemFactory {
         return Arrays.asList(names.split(SPLIT_DELIMITER));
     }
 
+
     private List<Participant> makeParticipantName(List<String> splitNames) {
         List<Participant> participants = new ArrayList<>();
         for (String splitName : splitNames) {
             participants.add(new Participant(splitName));
         }
         return participants;
+    }
+
+    private void validCountMoreThanOrEqualTo(List<String> splitNames) {
+        if (splitNames.size() < MORE_THAN_OR_EQUAL_TO_NUMBER) {
+            throw new IllegalArgumentException(String
+                .format(EXCEPTION_MESSAGE_FORMAT_LESS_THAN, splitNames.size(),
+                    MORE_THAN_OR_EQUAL_TO_NUMBER));
+        }
     }
 
     @Override
