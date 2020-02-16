ac051d942aa080069d2a6618b042ddc3521c672c
jamie9504 <jamie9504@kakao.com>
2020-02-16 20:24:49 +0900

    [테스트 추가] 사다리 높이가 0보다 크지 않으면 예외

--- src/test/java/domain/LadderHeightTest.java
+++ src/test/java/domain/LadderHeightTest.java
@@ -1,9 +1,12 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LadderHeightTest {
 
@@ -12,5 +15,14 @@ public class LadderHeightTest {
     void getHeight() {
         LadderHeight ladderHeight = new LadderHeight(5);
         assertThat(ladderHeight.getHeight()).isEqualTo(5);
+    }
+
+    @DisplayName("사다리 높이가 0보다 크지 않으면 아니면 예외")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void exceptionHeightNotMoreThan(int height) {
+        assertThatThrownBy(() -> new LadderHeight(height))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("1 이상 ");
     }
 }
