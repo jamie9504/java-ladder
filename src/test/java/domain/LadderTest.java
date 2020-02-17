55e9f976832d15ec6b1696258882ab930c43d3c8
jamie9504 <jamie9504@kakao.com>
2020-02-17 10:18:52 +0900

    [테스트 구현] 사다리 사이즈 검증

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -3,20 +3,35 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LadderTest {
+    private static LadderSize height;
+    private static LadderSize width;
+    private static Ladder ladderObject;
+
+    @BeforeAll
+    static void makeLadder() {
+        height = new LadderSize(5);
+        width = new LadderSize(7);
+        ladderObject = new Ladder(height, width);
+    }
 
     @DisplayName("사다리가져오기")
     @Test
     void getLadder() {
-        LadderSize height = new LadderSize(5);
-        LadderSize width = new LadderSize(7);
-        Ladder ladderObject = new Ladder(height, width);
         List<List<LegProperties>> ladder = ladderObject.getLadder();
         assertThat(ladder.get(0).get(0).getMoving()).isBetween(-1, 1);
         assertThat(ladder.get(4).get(4).getMoving()).isBetween(-1, 1);
+    }
 
+    @DisplayName("사다리 사이즈 검증")
+    @Test
+    void getLadderSize() {
+        ladderObject.getHeight();
+        assertThat(ladderObject.getHeight()).isEqualTo(height);
+        assertThat(ladderObject.getWidth()).isEqualTo(width);
     }
 }
