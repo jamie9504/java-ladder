9e65daed820a3f5917a1d5ab2e54b89f3eddf9b9
jamie9504 <jamie9504@kakao.com>
2020-02-17 10:33:04 +0900

    [기능 구현] OutputView 뼈대 입력

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,10 @@
+package view;
+
+public class OutputView {
+    static void outputLadder() {
+        System.out.println("사다리 게임을 실행합니다.");
+        // 참가자
+        // 사다리
+        // 당첨항목
+    }
+}
